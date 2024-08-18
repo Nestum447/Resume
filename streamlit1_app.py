9cb36d3786d6782a5a284e4ca9eca770c796a334
Nestum447 <ncarpio935@gmail.com>
2024-08-18 13:56:48 -0600

    Update streamlit1_app.py

--- streamlit1_app.py
+++ streamlit1_app.py
@@ -59,7 +59,7 @@ st.markdown("""
 <nav style="background-color: #16A2CB; position: fixed; top: 0; width: 100%; z-index: 1000;">
  <ul style="list-style-type: none; padding: 10px; display: flex; justify-content: space-around; margin: 0;">
     #<li><a style="text-decoration: none; color: white;" href="/">Home</a></li>
-    <li><a style="text-decoration: none; color: white;" href="#nestor-carpio">Inicio</a></li>
+    <li><a style="text-decoration: none; color: white;" href="#estrategica">Inicio</a></li>
     <li><a style="text-decoration: none; color: white;" href="#educacion">Educacion</a></li>
    <li><a style="text-decoration: none; color: white;" href="#experiencia-de-trabajo">Experiencia de Trabajo</a></li>
    <li><a style="text-decoration: none; color: white;" href="#bioinformatics-tools">Bioinformatics Tools</a></li>
