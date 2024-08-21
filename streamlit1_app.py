c64e98d3d3d858e5c6dca22769d00c39fe8f4f1f
Nestum447 <ncarpio935@gmail.com>
2024-08-20 21:54:21 -0600

    Update streamlit1_app.py

--- streamlit1_app.py
+++ streamlit1_app.py
@@ -112,7 +112,7 @@ nav ul {
 
 /* Asegura que el contenido no quede oculto debajo del menú */
 body {
-    padding-top: 120px; /* Ajusta el valor según la altura de tu menú */
+    padding-top: 200px; /* Ajusta el valor según la altura de tu menú */
 }
 </style>
 """, unsafe_allow_html=True)
